50d67b4787e60ef3b144760665459957f9ae60d9
Damien Radtke <admin@damienradtke.org>
2012-03-12 08:52:09 -0500

    Working some more.

--- hello.lisp
+++ hello.lisp
@@ -12,5 +12,25 @@
 
 ; display a window
 (gtk:within-main-loop
-  (let ((window (make-instance 'gtk:gtk-window :title "Hello, world!")))
+  ; Define the widgets
+  (let ((window (make-instance 'gtk:gtk-window
+							   :title "Notepad"
+							   :default-width 400
+							   :default-height 300
+							   :type :toplevel))
+		(textarea (make-instance 'gtk:text-view))
+		(menubar (make-instance 'gtk:menu-bar))
+		(vbox (make-instance 'gtk:v-box)))
+
+	; Fill the window
+	(gtk:box-pack-start vbox menubar :expand nil :fill nil :padding 0)
+	(gtk:box-pack-start vbox textarea :expand t :fill t :padding 0)
+	(gtk:container-add window vbox)
+
+	; Connect signals
+	(gobject:g-signal-connect window "delete-event" (lambda (window event)
+											  (declare (ignore window event))
+											  (gtk:gtk-main-quit)))
+	
+	; Finally, display everything
 	(gtk:widget-show window)))
